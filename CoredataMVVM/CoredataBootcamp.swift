d2d652e033a070b530f1ddd25ef4d70894b6527d
StudioKaori <67104053+StudioKaori@users.noreply.github.com>
2022-12-13 10:20:24 +0100

    Add: container

--- CoredataMVVM/CoredataBootcamp.swift
+++ CoredataMVVM/CoredataBootcamp.swift
@@ -13,8 +13,12 @@ class CoreDataViewModel: ObservableObject {
   let container: NSPersistentContainer
   
   init() {
+    // Set up the container
     container = NSPersistentContainer(name: "")
+    
     container.loadPersistentStores { description, error in
+      
+      // If the error occurs, print it
       if let error = error {
         print("Error loading core data: \(error)")
       }
